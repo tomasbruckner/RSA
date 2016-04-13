2993d1616c425e8693eecfd349ca54f11cdb9831
Tomas Bruckner <xbruck02@stud.fit.vutbr.cz>
2016-04-13 09:15:53 +0200

    pred smazanim pollard_rho

--- kry.c
+++ kry.c
@@ -22,9 +22,11 @@ int main (int argc, char** argv){
 	mpz_t result;	
 	mpz_init(result);
 
+    // generate
 	if(strcmp(argv[1], "-g") == 0){ 
 		rsa_generate_key( result, strtoul(argv[2], NULL, 0) );
     }
+    // encrypt or decrypt
 	else if( strcmp(argv[1], "-e") == 0 || strcmp(argv[1], "-d") == 0 ){
 		if(argc != 5) return 1;
 		mpz_t exponent, text, mod;
@@ -34,7 +36,8 @@ int main (int argc, char** argv){
 		
 		mpz_powm(result, text, exponent, mod);
 	    gmp_printf("%#Zx\n", result);
-	} 
+	}
+    // break
 	else if(strcmp(argv[1], "-b") == 0){
         mpz_t mod;
         mpz_init_set_str(mod, argv[2] + 2, 16);
@@ -49,11 +52,13 @@ int main (int argc, char** argv){
 }
 
 void rsa_break_key(mpz_t result, const mpz_t mod){
+    // even
     if(mpz_even_p(mod) != 0){
         mpz_set_ui(result, 0x2);
         return;
     }
 
+    // trial division
     for(int i = 0x3; i < 0xf4240; i = i + 2){
         if( mpz_mod_ui(result, mod, i) == 0){
             mpz_set_ui(result, i);
@@ -74,7 +79,8 @@ void rsa_generate_key(mpz_t result, const unsigned long bit){
     gmp_randstate_t state;
     gmp_randinit_default(state);
     gmp_randseed_ui(state, time(NULL));
-    
+
+    // even/odd check
     int offset = bit%2 == 0? 0 : 1;
     
     generate_prime(p, bit/2 + offset, state);
@@ -82,13 +88,17 @@ void rsa_generate_key(mpz_t result, const unsigned long bit){
 
 	gmp_printf("%#Zx ", p);
 	gmp_printf("%#Zx ", q);
+    
+    // modulus
     mpz_mul(tmp, p, q);
 	gmp_printf("%#Zx ", tmp);
 
+    // phi_n
     mpz_sub_ui(tmp, p, 0x1);
     mpz_sub_ui(phi_n, q, 0x1);
     mpz_mul(phi_n, phi_n, tmp);
 
+    // Fermat's primes
     const int exponents[] = { 0x3, 0x5, 0x11, 0x101, 0x10001 };
     for(int i = 0; i < 5; i++){
         mpz_set_ui(e, exponents[i]);
@@ -97,6 +107,8 @@ void rsa_generate_key(mpz_t result, const unsigned long bit){
     }
     
 	gmp_printf("%#Zx ", e);
+
+    // private key
 	gmp_printf("%#Zx\n", result);
 
     mpz_clear(p);
@@ -110,17 +122,19 @@ void rsa_generate_key(mpz_t result, const unsigned long bit){
 // http://crypto.stackexchange.com/questions/1970/how-are-primes-generated-for-rsa
 // http://crypto.stackexchange.com/questions/71/how-can-i-generate-large-prime-numbers-for-rsa
 void generate_prime(mpz_t result, const unsigned long bit, gmp_randstate_t state){
+    // generate random number greater 2
 	do{
 		mpz_urandomb(result, state, bit);
     }while( mpz_cmp_ui(result, 0x3) <= 0);
 
+    // if random number is even, make it odd
     if(mpz_even_p(result) != 0) mpz_add_ui(result, result, 0x1);
     
-    unsigned long f = 0, m = 0;
     int isprime = FALSE;
     while(1){
         isprime = FALSE;
         if(fermat_test(result, state)){
+            // one iteration has probability 1/2 of error
             for(int i = 0; i < 100; i++){
                 if( !miller_rabin_test(result, state) ){
                     isprime = FALSE;
@@ -131,14 +145,16 @@ void generate_prime(mpz_t result, const unsigned long bit, gmp_randstate_t state
         }
         
         if(isprime) break;
-        
+
+        // if not prime, add 2
         mpz_add_ui(result, result, 0x2);
     }
 }
 
 // http://blog.janmr.com/2009/10/computing-the-greatest-common-divisor.html
 void gcd_euclid(mpz_t result, const mpz_t op1, const mpz_t op2){
-    mpz_t tmp1, tmp2;
+    mpz_t   tmp1,   // op1 clone
+            tmp2;   // op2 clone
     mpz_init(tmp1);
     mpz_init(tmp2);
     mpz_set(tmp1, op1);
@@ -201,11 +217,12 @@ void update(mpz_t a, mpz_t b, const mpz_t y){
     mpz_init(tmp);
     mpz_init(tmp2);
     
+    // b = y*b - a
     mpz_set(tmp, b);
-
     mpz_mul(tmp2, y, tmp);
     mpz_sub(b, a, tmp2);
 
+    // a = b
     mpz_set(a, tmp);
 
     mpz_clear(tmp);
@@ -213,20 +230,29 @@ void update(mpz_t a, mpz_t b, const mpz_t y){
 }
 
 // http://mathcircle.berkeley.edu/BMC5/docpspdf/is-prime.pdf
-int fermat_test(const mpz_t prime, gmp_randstate_t state){
+int fermat_test(const mpz_t n, gmp_randstate_t state){
     int isprime = FALSE;
     mpz_t tmp, a, n_1;
     mpz_init(tmp);
     mpz_init(a);
     mpz_init(n_1);
-    mpz_sub_ui(n_1, prime, 0x1);
+    mpz_sub_ui(n_1, n, 0x1);
 
-    mpz_urandomm(a, state, prime);
-    gcd_euclid(tmp, a, prime);
-    if(mpz_cmp_ui(tmp, 1) > 0) isprime = FALSE;
-    
-	mpz_powm(tmp, a, n_1, prime);
-    isprime = mpz_cmp_ui(tmp, 0x1) == 0? TRUE: FALSE;
+    // generate a random a in {2,..., n-1}
+    do{
+        mpz_urandomm(a, state, n);
+    }while(mpz_cmp_ui(a, 0x2) < 0);
+
+    // tmp = gcd(a, n)
+    gcd_euclid(tmp, a, n);
+    if(mpz_cmp_ui(tmp, 1) > 0){
+        isprime = FALSE;
+    }
+    else{    
+        // tmp = a^(n-1) mod n
+	    mpz_powm(tmp, a, n_1, n);
+        isprime = mpz_cmp_ui(tmp, 0x1) == 0? TRUE: FALSE;
+    }
 
     mpz_clear(tmp);
     mpz_clear(a);
@@ -236,37 +262,54 @@ int fermat_test(const mpz_t prime, gmp_randstate_t state){
 }
 
 // http://mathcircle.berkeley.edu/BMC5/docpspdf/is-prime.pdf
-int miller_rabin_test(mpz_t prime, gmp_randstate_t state){
-    int isprime = FALSE, t = 0;
+int miller_rabin_test(mpz_t n, gmp_randstate_t state){
+    int isprime = FALSE;
     mpz_t m, a, b, n_1, tmp;
     mpz_init(a);
     mpz_init(b);
     mpz_init(m);
     mpz_init(tmp);
     mpz_init(n_1);
-    mpz_sub_ui(n_1, prime, 0x1);
+    mpz_sub_ui(n_1, n, 0x1);
+    
+    // generate a random a in {2,..., n-1}
     do{
-        mpz_urandomm(a, state, prime);
+        mpz_urandomm(a, state, n);
     }while(mpz_cmp_ui(a, 0x2) < 0);
 
+    // m = n-1
     mpz_set(m, n_1);   
-    t = 0;
+
+    int t = 0;
+
+    // factor n-1 as m*2^t 
     while(mpz_even_p(m) != 0){
         mpz_fdiv_q_2exp(m, m, 1);
         t++;
     }
-    mpz_powm(b, a, m, prime);
+    
+    // b = a^m mod n
+    mpz_powm(b, a, m, n);
+
+    // b == 1 then prime
     if(mpz_cmp_ui(b, 0x1) == 0) isprime = TRUE;
-    mpz_sub(tmp, b, prime);
+
+    // b == -1 then prime
+    // can be written as mpz_cmp(b, n_1); but is 50x slower
+    mpz_sub(tmp, b, n);
     if(mpz_cmp_si(tmp, -1) == 0) isprime = TRUE;
 
     if(!isprime){
         for(int i = t - 1; i != 0; i--){
-            mpz_powm_ui(b, b, 0x2, prime);
+            mpz_powm_ui(b, b, 0x2, n);
+            
+            // b == 1 then composite
             if(mpz_cmp_ui(b, 0x1) == 0){
                 break;
             }
-            mpz_sub(tmp, b, prime);
+
+            // b == -1 then prime
+            mpz_sub(tmp, b, n);
             if(mpz_cmp_si(tmp, -1) == 0){
                 isprime = TRUE;
                 break;
@@ -303,6 +346,7 @@ void pollard_rho(mpz_t result, const mpz_t n){
     mpz_set(n_1, n);
     mpz_sub_ui(n_1, n_1, 0x1);
 
+    // generate random x in {1,...,n-1}
     mpz_urandomm(x, state, n);
     mpz_add_ui(x, x, 0x1);
 
